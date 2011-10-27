dfb1d6def001042ac340e064a593733f83fd5f42
Chance Garcia <chance@storefrontconsulting.com>
2011-10-27 14:27:44 -0400

    SFC specific module builder for developer Chance Garcia

--- mage_module_creator.sh
+++ mage_module_creator.sh
@@ -10,18 +10,19 @@
 #	http://www.magentocommerce.com/wiki/custom_module_with_custom_database_table
 # License: http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
 
-NAMESPACE=""
+NAMESPACE="SFC"
 MODULE=""
+AUTHOR="Chance Garcia"
 
-while [[ "$NAMESPACE" == "" ]]; do
+while [ "$NAMESPACE" = "" ]; do
 	read -p "What is the Namespace you'd like to use? " NAMESPACE
 done
 
-while [[ "$MODULE" == "" ]]; do
+while [ "$MODULE" = "" ]; do
 	read -p "What is the name of your module? " MODULE
 done
 
-while [[ "$AUTHOR" == "" ]]; do
+while [ "$AUTHOR" = "" ]; do
 	read -p "What name should I use for author? " AUTHOR
 done
 
@@ -31,16 +32,16 @@ MODULE_LOWER=`echo $MODULE | awk '{print tolower($0)}'`
 
 echo "Creating module $PROJECT_NAME"
 
-if [[ -f 'skel.tar' ]]; then
+if [ -f 'skel.tar' ]; then
 	tar -xf skel.tar
 fi
 
-if [[ ! -d 'builds' ]]; then
+if [ ! -d 'builds' ]; then
 	mkdir builds
 fi
 
-if [[ -d 'skel' ]]; then
-	if [[ -d "builds/$PROJECT_NAME" ]]; then
+if [ -d 'skel' ]; then
+	if [ -d "builds/$PROJECT_NAME" ]; then
 		echo "Deleting previous build"
 		rm -fr "builds/$PROJECT_NAME"
 	fi
@@ -54,22 +55,22 @@ if [[ -d 'skel' ]]; then
 								sed "s/module/$MODULE_LOWER/g" | \
 								sed "s/foobar/modules/g"`
 		# Test if $LINE is a directory
-		if [[ -d $LINE ]]; then
+		if [ -d $LINE ]; then
 			#echo "Creating directory $BUILD"
 			mkdir -p $BUILD
 		else
 			# Test file type
 			echo $LINE | grep '\.xml$' > /dev/null 2>&1
-			if [[ $? -eq 0 ]]; then
+			if [ $? -eq 0 ]; then
 				# this is an XML file
-				cat $LINE | sed "s/ModuleCreator/$AUTHOR/g" | \
+				cat $LINE | sed "s/\[Author\]/$AUTHOR/g" | \
 							sed "s/\[Namespace\]/$NAMESPACE/g" | \
 							sed "s/\[namespace\]/$NAMESPACE_LOWER/g" | \
 							sed "s/\[Module\]/$MODULE/g" | \
 							sed "s/\[module\]/$MODULE_LOWER/g" | \
 							tr -d "\r" > $BUILD
 			else
-				cat $LINE | sed "s/ModuleCreator/$AUTHOR/g" | \
+				cat $LINE | sed "s/<Author>/$AUTHOR/g" | \
 							sed "s/<Namespace>/$NAMESPACE/g" | \
 							sed "s/<namespace>/$NAMESPACE_LOWER/g" | \
 							sed "s/<Module>/$MODULE/g" | \
@@ -84,7 +85,7 @@ else
 	exit 1
 fi
 
-if [[ -f builds/"$PROJECT_NAME".tar.gz ]]; then
+if [ -f builds/"$PROJECT_NAME".tar.gz ]; then
 	mv builds/"$PROJECT_NAME".tar.gz builds/"$PROJECT_NAME".tar.gz.old
 	echo "builds/$PROJECT_NAME.tar.gz moved to builds/$PROJECT_NAME.tar.gz.old"
 fi
